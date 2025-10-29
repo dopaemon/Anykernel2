5146d97d7ae3eeac51c7885a3eb3589eea54ef87
dopaemon <polarisdp@gmail.com>
2025-10-29 15:44:26 +0700

    anykernel2: setup slot
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- dora.sh
+++ dora.sh
@@ -6,8 +6,11 @@ CODENAME=$(getprop ro.product.device)
 # ui_print <text>
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
 
-ui_print "Hello World !!!";
-ui_print "${CODENAME}";
+# Print Device Codename
+ui_print " Device: ${CODENAME}";
+
+# Backup vendor_dlkm
+ui_print " Slot: ${SLOT}"
 
 # Remove Install file
 rm -rf /data/dora;
