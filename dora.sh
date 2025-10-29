b9dcdf6985e48eb215f04b262a9f4bae8c9c221d
dopaemon <polarisdp@gmail.com>
2025-10-29 14:35:01 +0700

    anykernel2: Maybe echo not work
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- dora.sh
+++ dora.sh
@@ -6,7 +6,8 @@ CODENAME=$(getprop ro.product.device)
 # ui_print <text>
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
 
-echo ${CODENAME}
+ui_print "Hello World !!!";
+ui_print "${CODENAME}";
 
 # Remove Install file
 rm -rf /data/dora;
